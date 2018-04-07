6dd9add717ea9f6c081fef6cd3043976d6741438
Rune Madsen <rune@runemadsen.com>
2018-04-07 17:46:48 +0200

    adding css grid

---
+++ week-11/grid/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>Header</header>
+        <aside>Sidebar</aside>
+        <main>Main</main>
+      </div>
+    );
+  }
+}
+
+export default App;
